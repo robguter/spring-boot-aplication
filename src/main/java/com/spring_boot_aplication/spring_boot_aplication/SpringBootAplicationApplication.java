d1a03ac3bcd69f383c10b28c3eddd4401d31d43f
Robert Gutierrez <robgutgom@hotmail.com>
2021-06-30 22:50:52 -0400

    first commit

---
+++ src/main/java/com/spring_boot_aplication/spring_boot_aplication/SpringBootAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_aplication.spring_boot_aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_aplication/spring_boot_aplication/SpringBootAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_aplication.spring_boot_aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
